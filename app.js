080870c82bf42c1709ec31d4292a03a4473e3e4b
Jackie ngo <jackie.ngo888@gmail.com>
2018-01-06 11:59:00 -0500

    changed server to read from new website folder

--- app.js
+++ app.js
@@ -20,11 +20,22 @@ app.get('/the-team', function(req, res) {
 	res.redirect('/team_website.html');
 });
 
+app.get('/steam_logo', function(req, res) {
+	console.log('image');
+	res.redirect('/steam_logo');
+});
+
 app.use(function(req, res, next) {
   console.log('Someone just visited the webpage.');
   next();
 });
 
-app.use( express.static(__dirname + '/SteamWebsite'));
+
+
+
+
+app.use('/',  express.static(__dirname + '/SteamWebsite2.0'));
+
+//app.use('/images', express.static(__dirname +'/images'));
 
 app.listen(3000);
